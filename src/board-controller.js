f3a8c323ab3061374da1239b13461e0dca34c23f
Erik Öjebo <erik.ojebo@gmail.com>
2014-11-05 23:12:50 +0100

    improved styling for the small board

--- src/board-controller.js
+++ src/board-controller.js
@@ -1,18 +1,14 @@
-angular.module("alphamemo").controller("boardController", function ($scope) {
+angular.module("alphamemo").controller("boardController", function ($scope, $routeParams) {
 
-    var tiles = [{
-        value: 'A'
-    },{
-        value: 'B'
-    },{
-        value: 'A'
-    },{
-        value: 'B'
-    },{
-        value: 'C'
-    },{
-        value: 'C'
-    }];
+    var tiles = [];
+
+    var tileCount = $routeParams.tileCount;
+
+    for (var i = 0; i < tileCount; i++) {
+        tiles.push({
+            value: String.fromCharCode(65 + (i % (tileCount / 2)))
+        });
+    }
 
     $scope.tiles = tiles;
 });
--- src/menu-controller.js
+++ src/menu-controller.js
@@ -1,3 +1,24 @@
 angular.module("alphamemo").controller("menuController", function ($scope) {
     $scope.tileCount = 0;
+
+    $scope.tileSetups = [{
+        imageSource: "images/small_board.png",
+        tileCount: 6
+    }, {
+        imageSource: "images/medium_board.png",
+        tileCount: 12
+    }, {
+        imageSource: "images/large_board.png",
+        tileCount: 20
+    }];
+    
+    $scope.selectTileSetup = function (tileSetup) {
+        $scope.tileCount = tileSetup.tileCount;
+
+        for (var i = 0; i < $scope.tileSetups.length; i++) {
+            $scope.tileSetups[i].isSelected = false;
+        }
+
+        tileSetup.isSelected = true;
+    };
 });
