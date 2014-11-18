8dfd532823ad0ccf227ef28f7c89f495623b575f
Erik Öjebo <erik.ojebo@gmail.com>
2014-11-18 22:04:07 +0100

    Implemented all game types and fixed ipad styling issue

--- src/js/board/tile-factory.js
+++ src/js/board/tile-factory.js
@@ -26,8 +26,58 @@ memo.board.createTileValuePairs = (function () {
         return pairs;
     }
 
+    function createLowercaseValuePairs() {
+        var pairs = [];
+
+        var lowercaseStartIndex = 'a'.charCodeAt(0);
+        var lowercaseEndIndex = 'z'.charCodeAt(0);
+
+        for (var i = lowercaseStartIndex; i < lowercaseEndIndex; i++) {
+            var letter = String.fromCharCode(i);
+            pairs.push(createValuePair(letter, letter));
+        }
+
+        return pairs;
+    }
+
+    function createUppercaseValuePairs() {
+        var pairs = [];
+
+        var uppercaseStartIndex = 'A'.charCodeAt(0);
+        var uppercaseEndIndex = 'Z'.charCodeAt(0);
+
+        for (var i = uppercaseStartIndex; i < uppercaseEndIndex; i++) {
+            var letter = String.fromCharCode(i);
+            pairs.push(createValuePair(letter, letter));
+        }
+
+        return pairs;
+    }
+    
+    function createNumberValuePairs(tileCount) {
+        var pairs = [];
+
+        for (var i = 0; i < tileCount; i++) {
+            pairs.push(createValuePair(i, i));
+        }
+
+        return pairs;
+    }
+    
     return function(gameType, tileCount) {
-        var valuePairs = createMixedcaseValuePairs();
+        var valuePairs = null;
+
+        gameType = gameType.toLowerCase();
+
+        if (gameType == "mixedcase") {
+            valuePairs = createMixedcaseValuePairs();
+        } else if (gameType == "lowercase") {
+            valuePairs = createLowercaseValuePairs();
+        } else if (gameType == "numbers") {
+            valuePairs = createNumberValuePairs(tileCount);
+        } else {
+            valuePairs = createUppercaseValuePairs();
+        }
 
         return _.take(_.shuffle(valuePairs), tileCount / 2);
     };
